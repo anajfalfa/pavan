651e9d17c757ff7e2aef844a31d891162788ef0b
anajfalfa <anajfalfaiate@gmail.com>
2025-03-13 08:23:45 +0000

    new files; statement

---
+++ Exceptional.jl
@@ -0,0 +1,35 @@
+#=
+PROJECT | Advanced Programming Course (2024/2025)
+Goal: implementation, in the Julia programming language, of the operations for 
+the signaling and handling of exceptional situations, including the use of restarts
+
+By: Ana Alfaiate 102903 ; Inês Trigueiro 102902 ; Raquel Coelho 102881
+=#
+function to_escape(func)
+
+end
+
+function handling(func, handlers...)
+
+end
+
+function with_restart(func, restarts...)
+
+end
+
+
+function available_restart(name)
+
+end
+
+
+function invoke_restart(name, args...)
+end
+
+function signal(exception)
+
+end    
+
+function error(exception)
+
+end
---
+++ ExceptionalExtended.jl
@@ -0,0 +1,13 @@
+#=
+ Examples of interesting extensions include:
+ ˆ
+ ˆ
+ ˆ
+ Implementing user-handling of restarts, as is possible in Common Lisp by interacting with
+ the user, presenting the available restarts and allowing him to choose the preferred one.
+ Implementing the Julia equivalent to the Common Lisp restart options :test, :report, and
+ :interactive.
+ Implementing the macros handler_case and restart_case to simplify the use of the func
+tions handling and with_restart.
+
+=#
